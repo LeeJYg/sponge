d2abf86927f4a00d320065344fd3a376f8403ad7
LeeJYg <92172520+LeeJYg@users.noreply.github.com>
2023-03-19 13:58:02 +0900

    Update tcp_receiver.hh

--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -16,7 +16,7 @@
 class TCPReceiver {
     //! Our data structure for re-assembling bytes.
     StreamReassembler _reassembler;
-    WrappingInt32 _isn{0};
+    optional<WrappingInt32> _isn{0};
     uint64_t _checkpoint = 0;
 
     //! The maximum number of bytes we'll store.
