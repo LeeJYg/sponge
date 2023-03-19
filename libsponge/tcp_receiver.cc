cc45e4c60c45f9d3124053d0c19a152efa542cdf
LeeJYg <92172520+LeeJYg@users.noreply.github.com>
2023-03-19 13:58:47 +0900

    Update tcp_receiver.cc

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -15,18 +15,21 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
         _isn = WrappingInt32{seg.header().seqno.raw_value()};
     }
 
-    int64_t abo_seqno = unwrap(seg.header().seqno + static_cast<int>(seg.header().syn), _isn, _checkpoint);
+    int64_t abo_seqno = unwrap(seg.header().seqno + static_cast<int>(seg.header().syn), _isn.value(), _checkpoint);
 
     _reassembler.push_substring(seg.payload().copy(), abo_seqno - 1, seg.header().fin);
     _checkpoint += seg.length_in_sequence_space();
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if(_isn == nullopt){
+        return nullopt;
+    }
     uint64_t written = stream_out().bytes_written () + 1;
     if(stream_out().input_ended()){
         written += 1;
     }
-    return wrap(written, _isn);
+    return wrap(written, _isn.value());
 }
 
 size_t TCPReceiver::window_size() const { 
